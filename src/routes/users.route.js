49df1c38b9eb7528858ba1ae44fc9de592391692
Gakiri001 <gakirijulius@gmail.com>
2024-06-21 16:40:30 +0300

    implement the get all users route

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,16 @@
 import {Router} from 'express'
+import pool from '../db.config.js';
 
 const router = Router();
 
-router.get("/",(req,res) => {
-  res.send("Getting all users")
+router.get("/",async (req,res) => {
+  try{
+    const result = await pool.query("SELECT * FROM usersTable");
+    res.status(200).json({success:true,data:result.rows})
+  }
+  catch(err){
+    res.status(500).json({success:false,message:err.message})
+  }
 })
 
 router.get("/:id",(req,res) => {
