b7a5d534dd362013db7338386f386641006417c9
Gakiri001 <gakirijulius@gmail.com>
2024-06-22 07:57:53 +0300

    Get single user route

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -13,12 +13,24 @@ router.get("/",async (req,res) => {
   }
 })
 
-router.get("/:id",(req,res) => {
-  res.send("Getting a single user")
+router.get("/:id",async (req,res) => {
+  const id = req.params.id;
+  try{
+    const result = await pool.query("SELECT * FROM usersTable WHERE id=$1",[id]);
+    if(result.rowCount === 0){
+      res.status(404).json({success:false, message:"User not found"})
+    }
+    else{
+      res.status(200).json({success:true,data:result.rows[0]})
+    }
+  }
+  catch(err){
+    res.status(500).json({success:false, message:err.message})
+  }
 })
 
-router.post("/",(red,res) => {
-  res.send("Creating a user")
+router.post("/",async (req,res) => {
+  res.send("Post user to database")
 })
 
 router.patch("/:id",(req,res) => {
